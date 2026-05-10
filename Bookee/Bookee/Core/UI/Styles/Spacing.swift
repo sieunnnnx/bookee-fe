36e8f45d2930b3d00fe0210505f467ac911e08d2
sieunnnnx <sieunnnnx@gmail.com>
2026-05-10 13:10:16 +0900

    chore: spacing 공통 스타일 정의 (#3)

---
+++ Bookee/Bookee/Core/UI/Styles/Spacing.swift
@@ -0,0 +1,21 @@
+//
+//  Spacing.swift
+//  Bookee
+//
+//  Created by sieunnnx on 5/10/26.
+//
+
+import Foundation
+
+enum AppSpacing {
+    
+    static let spacing2 : CGFloat = 2
+    static let spacing4 : CGFloat = 4
+    static let spacing6 : CGFloat = 6
+    static let spacing8 : CGFloat = 8
+    static let spacing12 : CGFloat = 12
+    static let spacing16 : CGFloat = 16
+    static let spacing24 : CGFloat = 24
+    static let spacing28 : CGFloat = 28
+    static let spacing32 : CGFloat = 32
+}
