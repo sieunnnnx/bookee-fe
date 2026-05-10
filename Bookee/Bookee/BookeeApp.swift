1a9bee7acae3f184294b841955cbf797dacace69
sieunnnnx <sieunnnnx@gmail.com>
2026-05-10 12:32:17 +0900

    chore: 프로젝트 생성 및 초기설정 (#2)
    * setting: .gitignore 파일 생성 (#1)
    
    * docs: 이슈 템플릿 생성 (#1)
    
    * setting: 프로젝트 생성 및 리소스 추가 (#1)

---
+++ Bookee/Bookee/BookeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookeeApp.swift
+//  Bookee
+//
+//  Created by sieunnnx on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BookeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
