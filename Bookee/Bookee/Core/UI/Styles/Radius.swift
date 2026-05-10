5cfe8f654b5008ae0f80b515852b34e5c7471214
sieunnnnx <sieunnnnx@gmail.com>
2026-05-10 13:12:33 +0900

    chore: radius 공통 스타일 정의 (#3)

---
+++ Bookee/Bookee/Core/UI/Styles/Radius.swift
@@ -0,0 +1,20 @@
+//
+//  Radius.swift
+//  Bookee
+//
+//  Created by sieunnnx on 5/10/26.
+//
+
+import Foundation
+
+enum Radius {
+    
+    static let radius2: CGFloat = 2
+    static let radius4: CGFloat = 4
+    static let radius6: CGFloat = 6
+    static let radius8: CGFloat = 8
+    static let radius12: CGFloat = 12
+    static let radius16: CGFloat = 16
+    static let radius20: CGFloat = 20
+    static let radius24: CGFloat = 24
+}
